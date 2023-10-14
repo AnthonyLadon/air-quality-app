431e19c7ed7547208a7673b2b01c7d7a31437af4
Anthony Ladon <ladonanthony@gmail.com>
2023-10-14 15:47:20 +0200

    color changings

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Fetch data from API (airvisual.com)
 async function getCityDatas() {
   // DOM elements
+  let background = document.getElementById("background-layer");
   let moodIcon = document.getElementById("mood-icon");
   let citySituation = document.getElementById("city-situation");
   let pollutionStatus = document.getElementById("pollution-status");
@@ -8,6 +9,7 @@ async function getCityDatas() {
   let temperatureStatus = document.getElementById("temperature-status");
   let humidityStatus = document.getElementById("humidity-status");
   let windSpeedStatus = document.getElementById("wind-speed-status");
+  let cursor = document.getElementById("pollution-cursor");
 
   try {
     const response = await fetch(
@@ -35,31 +37,43 @@ async function getCityDatas() {
         pollutionStatus.textContent = "Bon";
         moodIcon.src = "./ressources/happy.svg";
         moodIcon.alt = "happy smiley icon";
+        cursor.style.left = "0%";
+        background.style.backgroundColor = "#00ff007a";
         break;
       case cityData.pollution >= 51 && cityData.pollution < 100:
         pollutionStatus.textContent = "Moyen";
         moodIcon.src = "./ressources/thinking.svg";
         moodIcon.alt = "thinking smiley icon";
+        cursor.style.left = "15%";
+        background.style.backgroundColor = "#ffff007a";
         break;
       case cityData.pollution >= 101 && cityData.pollution <= 150:
         pollutionStatus.textContent = "Mauvais";
         moodIcon.src = "./ressources/bad.svg";
         moodIcon.alt = "bad smiley icon";
+        cursor.style.left = "25%";
+        background.style.backgroundColor = "#ff7f007a";
         break;
       case cityData.pollution >= 151 && cityData.pollution <= 200:
         pollutionStatus.textContent = "Dangereux";
         moodIcon.src = "./ressources/unhealthy.svg";
         moodIcon.alt = "unhealthy smiley icon";
+        cursor.style.left = "35%";
+        background.style.backgroundColor = "#ff00007a";
         break;
       case cityData.pollution >= 201 && cityData.pollution <= 300:
         pollutionStatus.textContent = "Trés dangereux";
         moodIcon.src = "./ressources/terrible.svg";
         moodIcon.alt = "terrible smiley icon";
+        cursor.style.left = "50%";
+        background.style.backgroundColor = "#8b00ff7a";
         break;
       case cityData.pollution >= 301 && cityData.pollution <= 500:
         pollutionStatus.textContent = "Extremmement dangereux";
         moodIcon.src = "./ressources/masked.svg";
         moodIcon.alt = "masked smiley icon";
+        cursor.style.left = "80%";
+        background.style.backgroundColor = "#7e00237a";
       default:
         pollutionStatus.textContent = "Pas de données";
         moodIcon.src = "./ressources/magnifying-glass.svg";
@@ -70,4 +84,4 @@ async function getCityDatas() {
   }
 }
 
-//getCityDatas();
+getCityDatas();
