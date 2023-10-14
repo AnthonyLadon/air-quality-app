525f92d6bfcde3b225d798a1285077ce5968392e
Anthony Ladon <ladonanthony@gmail.com>
2023-10-14 14:19:50 +0200

    fetch API data

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-// 76fa60d1-6fdc-4ef9-9a0c-9dfa19eb9531
+async function getCityDatas() {
+  try {
+    const response = await fetch(
+      "https://api.airvisual.com/v2/nearest_city?key=76fa60d1-6fdc-4ef9-9a0c-9dfa19eb9531"
+    );
+    const responseData = await response.json();
+    console.log(responseData);
 
-// http://api.airvisual.com/v2/nearest_city?key={{YOUR_API_KEY}}
+    const cityData = {
+      city: responseData.data.city,
+      pollution: responseData.data.current.pollution.aqius,
+      temperature: responseData.data.current.weather.tp,
+      humidity: responseData.data.current.weather.hu,
+      windSpeed: responseData.data.current.weather.ws,
+    };
+    console.log(cityData);
+
+    return responseData;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getCityDatas();
